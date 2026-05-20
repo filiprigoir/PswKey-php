e9b8f3dc6675c899c63663cf4afbc71e92a05d44
Filip Rigoir <filip.rigoir@telenet.be>
2026-05-20 14:14:39 +0200

    optimze comment into readme

--- tests/ReadmeTest.php
+++ tests/ReadmeTest.php
@@ -14,17 +14,12 @@ class ReadmeTest extends TestCase
 {
     public function test_readme_common_usage() : void {
 
-        //key change in time
         $date = new DateTime();
         $key = \strtotime($date->format('Y-m-d H:i:s')) . $date->format('u'); 
 
-        //Derived Provider
         $keyStream = new KeyStream("Service=README.md : user=536984 : alias=VisalStudio", $key);
 
-        //Deterministic converter
         $pswkey = new PswKey($keyStream);
-
-        //Grab 100,000 random bytes
         $randomBytes = random_bytes(100_000);
 
         //Conversion
@@ -52,14 +47,6 @@ class ReadmeTest extends TestCase
                     $randomBytes
                 );
             }
-            else {
-                //Message for users and client(public)
-                echo $status->clientMessage;
-            }
-        }
-        else {
-            //Message for devs/logs (protected)
-            echo $status->internalMessage;
         }
     }
 
@@ -67,7 +54,6 @@ class ReadmeTest extends TestCase
 
         try {
 
-        //User credentials
         $email = "info@pswkey.com";
         $password = "My_PswKeyµ2025!";
 
@@ -76,10 +62,8 @@ class ReadmeTest extends TestCase
             "Service=README.md | emailadress={$email}" //Seedphrase can be anything related to your service
         );
 
-        //Attach password as custom key
         $keyStream->setCustomKey($password);
 
-        //Generated outputs
         $transportkey = "";
         $passwordHash = "";
         $keyStream->byteStream(
@@ -87,37 +71,19 @@ class ReadmeTest extends TestCase
                 
                 $pswKey = new PswKey($keyStream);
 
-                //Derived password => transport-safe Base32 representation
                 $transportkey = $pswKey->from(256)->to(32)->convert($derivedPassword);
-
-                //Derived password => password hash for database storage
                 $passwordHash = password_hash($derivedPassword, PASSWORD_DEFAULT);
 
                 unset($pswKey);
             },
-            64, //Derived password length
-            "MySecret" //Your service-specific derivation context (most be 8 bytes)
+            64, 
+            "MySecret"
             );
 
             $this->assertNotEmpty($transportkey);
             $this->assertNotEmpty($passwordHash);
-
-            //To validate:
-            //regenerate the "derived password" from the incoming password
-            //and compare it with the stored database hash
-
-            //Example password hash:
-            //$2y$10$0qbuEgpR/RRn4a2xj3LHu.eHH288.615tSGkBd.4kIYlyddwpdhvO
-
-            //Optional "transport key" representation:
-            //(cookies, transport identifiers, login flows, etc.)
-
-            //Example transport key:
-            //gUi0zrccwPdLL88IvwUwB2eMO2UtP0UvvNcITTgOv8cNl0W0o8lgPwQtoNTTlTgAA8l0P2vBl2WtAUtwANtOfIpoovpyO2
-            //UfOlUTWMUHBNvicA2OfvzfO8UANeoz
         }
         finally {
-            //Clear sensitive data
             Memezero::overwriteString($password);
             unset($keyStream);   
         }
@@ -126,28 +92,21 @@ class ReadmeTest extends TestCase
     public function test_readme_validate_usage() : void {
 
         try {
-            //User credentials
             $email = "info@pswkey.com";
 
             //Incoming transport key
             $transportKey = "gUi0zrccwPdLL88IvwUwB2eMO2UtP0UvvNcITTgOv8cNl0W0o8lgPwQtoNTTlTgAA8l0P2vBl2WtAUtwANtOfIpoovpyO2"
                 . "UfOlUTWMUHBNvicA2OfvzfO8UANeoz";
 
-            //Derived stream provider
             $keyStream = new KeyStream("Service=README.md | emailadress={$email}");
-
             $pswKey = new PswKey($keyStream);
 
-            //Decoded transport key into derived password bytes
             $bytePassword = $pswKey->from(32)->to(256)->convert($transportKey);
 
-            //Validate transport key conversion
             if($bytePassword !== null) {
                 
-                //Retrieve stored password hash only after successful transport-key validation
                 $dbHashed = "\$2y\$10\$0qbuEgpR/RRn4a2xj3LHu.eHH288.615tSGkBd.4kIYlyddwpdhvO";
 
-                //Compare derived password against stored hash
                 $status = "";
                 if (password_verify($bytePassword, $dbHashed)) {
                     $status = "Password is valid!";
@@ -162,7 +121,6 @@ class ReadmeTest extends TestCase
             }
         }
         finally {
-            //Clear sensitive data
             Memezero::overwriteString($bytePassword);
             unset($keyStream, $pswKey);   
         }
@@ -171,20 +129,15 @@ class ReadmeTest extends TestCase
     public function test_readme_otp_usage() : void {
 
         try {
-            //Digit number
             $originalDigits = "0931024538975689521014785";
 
-            //Time-based key
             $date = new DateTime();
             $key = \strtotime($date->format('Y-m-d H:i:s')) . $date->format('u');
 
             //Derived stream provider
             $keyStream = new KeyStream("Service=README.md : user=5236947 : alias=VisalStudio", $key);
 
-            //OTP instance
             $oneTimePad = new OneTimePad($keyStream);
-
-            //Using any numeric identifier and 8-byte derivation context
             $encode = $oneTimePad->digit($originalDigits, 5236947, "MyDigits");
 
             $this->assertNotEquals(
@@ -195,7 +148,6 @@ class ReadmeTest extends TestCase
             $status = $oneTimePad->status();
             if($status->valid) {
 
-                //Decode into orginal digits ith same numeric identifier and 8-byte derivation context
                 $decode = $oneTimePad->digit($encode, 5236947, "MyDigits");
 
                 $this->assertEquals(
@@ -203,12 +155,8 @@ class ReadmeTest extends TestCase
                     $originalDigits
                 );
             }
-            else {
-                echo $status->internalMessage;
-            }
         }       
         finally {
-            //Clear sensitive data
             Memezero::overwriteString($originalDigits);
             unset($keyStream, $oneTimePad); 
         }
